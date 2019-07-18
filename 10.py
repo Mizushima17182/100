a34e503efd5ff27f317ac60376fc8ad338860f6e
mizushima <51814668+Mizushima17182@users.noreply.github.com>
2019-07-18 17:17:45 +0900

    Add files via upload
    test

--- 10.py
+++ 10.py
@@ -1,8 +1,15 @@
 import sys
-#unix $ wc -l hightemp.txt
+hight = sys.argv
+print(hight)
+file = open("hightemp.txt","r",encoding="utf-8")
+print(file.readline())
 
-data = open("hightemp.txt","r",encoding="utf-8")
-print(data.readline())
+print(len(file.readline()))
+#wc -1 hightemp.txt
 
-print(len(data.readline()))
-#wc hightemp.txt
+# ========
+# コメント
+# ========
+# L3: openはデータというよりもファイルオブジェクト的なものになるので、変数名はfileのほうが良いと思う
+# L1: argvを使おう（以降のファイル全部）
+# wc -l hightemp.txt かな
